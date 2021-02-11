62efd4e1bfdc3ddfa6166205e10f32b02be60d6d
Kewen Zhao <Kevin@Kewens-MacBook-Pro.local>
2021-02-11 05:15:14 -0800

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,25 +4,33 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // TODO
   localStorage = window.localStorage;
-  if (localStorage.getItem('count') === null ){
+  if (localStorage.getItem('count') === null) {
     localStorage.setItem('count', 0);
-  } else{
+  } else {
     var count = document.getElementById("cart-count");
     count.textContent = localStorage.getItem('count');
   }
-  if (localStorage.getItem('items') === null ) {
+  if (localStorage.getItem('items') === null) {
+    console.log("fetching...");
     fetch('https://fakestoreapi.com/products')
-    .then(response => response.json() )
-    .then(data => localStorage.setItem('items', JSON.stringify(data)));
-  }
-  data = JSON.parse(localStorage.getItem('items'));
-  //console.log(data[0]);
-  //data = data.json();
-  container = document.getElementById('product-list');
-  for(let i = 0; i < data.length; i++){
-    let child = new ProductItem(data[i].id, data[i].image, data[i].title, data[i].title, data[i].price);
-    //console.log(data[i].image);
-
-    container.appendChild(child);
-  }
-});
+      .then(response => response.json())
+      .then(data => {
+          console.log("finished fetching!");
+          localStorage.setItem('items', JSON.stringify(data));
+          container = document.getElementById('product-list');
+          for (let i = 0; i < data.length; i++) {
+            let child = new ProductItem(data[i].id, data[i].image, data[i].title, data[i].title, data[i].price);
+            container.appendChild(child);
+          }
+        });
+      }
+    else {
+      data = localStorage.getItem('items');
+      container = document.getElementById('product-list');
+      for (let i = 0; i < data.length; i++) {
+        let child = new ProductItem(data[i].id, data[i].image, data[i].title, data[i].title, data[i].price);
+        container.appendChild(child);
+      }
+      //console.log(data[0]);
+    }
+  } );
